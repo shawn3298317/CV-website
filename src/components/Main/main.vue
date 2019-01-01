bc7daa908f285f102d8942a17836eb239425597b
shawn3298317 <shawn3298317lin@gmail.com>
2019-01-01 22:19:38 +0800

    Update project content & Change main page background to static img

--- src/components/Main/main.vue
+++ src/components/Main/main.vue
@@ -4,8 +4,8 @@
     <annouceBar></annouceBar>
 
     <div class="nav-bar" style="overflow: hidden;">
-      <iframe id="player" frameborder="0" allowfullscreen="1" allow="autoplay; encrypted-media" title="YouTube video player" width="560" height="315" src="https://www.youtube.com/embed/CJny8XlzcDU?autohide=1&amp;autoplay=1&amp;controls=0&amp;enablejsapi=1&amp;iv_load_policy=3&amp;loop=0&amp;modestbranding=1&amp;playsinline=1&amp;rel=0&amp;showinfo=0&amp;wmode=opaque&amp;origin=https%3A%2F%2Fwww.devonstank.com&amp;widgetid=1" class="background-video2 ready" style="width: 100%; height: 737px;"></iframe>
-      <!-- <img class="background-video2" src="../../../static/images/bg_video.jpeg" width="560" height="315" style="width: 1310.22px; height: 737px;"> -->
+      <!-- <iframe id="player" frameborder="0" allowfullscreen="1" allow="autoplay; encrypted-media" title="YouTube video player" width="560" height="315" src="https://www.youtube.com/embed/CJny8XlzcDU?autohide=1&amp;autoplay=1&amp;controls=0&amp;enablejsapi=1&amp;iv_load_policy=3&amp;loop=0&amp;modestbranding=1&amp;playsinline=1&amp;rel=0&amp;showinfo=0&amp;wmode=opaque&amp;origin=https%3A%2F%2Fwww.devonstank.com&amp;widgetid=1" class="background-video2 ready" style="width: 100%; height: 737px;"></iframe> -->
+      <img class="background-video2" src="../../../static/images/bg_video.jpeg" width="560" height="315" style="width: 1310.22px; height: 737px;">
 
       <navBar></navBar>
 
